83ab37e676ec5099d87b96df4418d76a63ea15c1
GulsanamSadirdinova <sadirdinovagulsanam40@gmail.com>
2025-10-16 06:18:14 +0300

    loyiha

--- src/components/gulsanam/Gulsanam.jsx
+++ src/components/gulsanam/Gulsanam.jsx
@@ -1,11 +1,38 @@
+import { Link } from "react-router-dom";
+import { products } from "../../mock/Index";
+import { IoIosStar } from "react-icons/io";
+import "./Gulsaanm.css"
+
 import React from 'react'
 
 const Gulsanam = () => {
   return (
     <div>
-      <h2>
-        hi
-      </h2>
+
+
+    <div className="product">
+      <div className="product-wrap">
+        {products.map((item, index) => {
+          return (
+            <div key={index} className="product-card">
+              <Link to={`/product/${item.id}`}>
+                <img src={item.img} alt="" />
+              </Link>
+              <h2>{item.title}</h2>
+              <div>
+
+              <IoIosStar className="yulduz"/>
+              <IoIosStar className="yulduz"/>
+              <IoIosStar className="yulduz"/>
+              </div>
+              <b className="price">{item.price}</b>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  
+
     </div>
   )
 }
