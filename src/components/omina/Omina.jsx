404bb5b297c1a2c43db968b79989c36de7c0e0b0
ominaxon7 <abobakirovaominaxon@gmail.com>
2025-10-16 09:58:56 +0500

    vazifa

--- src/components/omina/Omina.jsx
+++ src/components/omina/Omina.jsx
@@ -1,3 +1,6 @@
+
+
+
 import "./Omina.css"
 import image from '../../assets/shop.co1.png'
 import logo from "../../assets/SHOP.CO3.png"
@@ -5,72 +8,78 @@ import { FaSearch } from "react-icons/fa";
 import { SlBasket } from "react-icons/sl";
 import { FaRegUserCircle } from "react-icons/fa";
 
-
 const Omina = () => {
   return (
     <div className='container'>
       <div className="first-line">
-      <p>Sign up and get 20% off to your first order. Sign Up Now</p>
+        <p>Sign up and get 20% off to your first order. Sign Up Now</p>
       </div>
+
+      {/* 🔹 Navbar */}
       <div className="navbar">
-        <img src={logo} alt="" />
+        <img src={logo} alt="logo" className="logo" />
+        
         <div className="links-wr">
-        <p>Shop </p>
-        <p>On Sale</p>
-        <p>New Arrivals</p>
-        <p>Brands</p>
+          <p>Shop</p>
+          <p>On Sale</p>
+          <p>New Arrivals</p>
+          <p>Brands</p>
         </div>
+        
         <div className="input-wrap">
-        <button><FaSearch /></button>
-        <input placeholder='Search for products...' type="text" />
+          <button><FaSearch /></button>
+          <input placeholder='Search for products...' type="text" />
         </div>
+
         <div className="icons-wr">
-        <SlBasket />
-        <FaRegUserCircle />
+          <SlBasket />
+          <FaRegUserCircle />
         </div>
       </div>
-      <div style={{backgroundColor:"#F2F0F1", display:"flex", justifyContent:"space-between", flexWrap:"wrap",alignItems:"center"}}>
-  <div className='left-wr'>
-    <h2 className='site-title'>FIND  CLOTHES  THAT  MATCHES  YOUR  STYLE</h2>
-    <p className='banner-desc'>Browse through our diverse range of meticulously crafted garments, designed to biring out your individuality and cater to your sense of style. </p>
-    <button className='banner-btn'>Shop Now</button>
-    <div className='numbers-wr'>
-      <div className='first-num'>
-        <h3 className='num'>200+</h3>
-        <p style={{color:"#00000099", fontSize:"25px",width:"256px"}}>International Brands</p>
+
+      {/* 🔹 Banner */}
+      <div className="banner">
+        <div className='left-wr'>
+          <h2 className='site-title'>FIND CLOTHES THAT MATCHES YOUR STYLE</h2>
+          <p className='banner-desc'>
+            Browse through our diverse range of meticulously crafted garments,
+            designed to bring out your individuality and cater to your sense of style.
+          </p>
+          <button className='banner-btn'>Shop Now</button>
+
+          <div className='numbers-wr'>
+            <div className='first-num'>
+              <h3 className='num'>200+</h3>
+              <p>International Brands</p>
+            </div>
+
+            <div className='first-num'>
+              <h3 className='num'>2,000+</h3>
+              <p>High-Quality Products</p>
+            </div>
+
+            <div className='first-num'>
+              <h3 className='num'>30,000+</h3>
+              <p>Happy Customers</p>
+            </div>
+          </div>
+        </div>
+
+        <div className='right-wr'>
+          <img className='site-img' src={image} alt="banner" />
+        </div>
       </div>
 
-      <div className='first-num'>
-        <h3 className='num'>2,000+</h3>
-        <p style={{color:"#00000099",fontSize:"25px",width:"256px"}}>High-Quality Products</p>
+      {/* 🔹 Brands */}
+      <div className='bottom-wr'>
+        <h3>VERSACE</h3>
+        <h3>ZARA</h3>
+        <h3>GUCCI</h3>
+        <h3>PRADA</h3>
+        <h3>CALVIN KLEIN</h3>
       </div>
-
-
-      <div className='first-num'>
-        <h3 className='num'>30,000+</h3>
-        <p style={{color:"#00000099", fontSize:"25px", width:"256px"}}>Happy Customers</p>
-      </div>
-    
-    </div>
-    </div>
-
-<div className='right-wr'>
-  <img className='site-img' src={image} alt="" />
-</div>
-</div>
-
-<div className='bottom-wr'>
-  <h3>VERSACE</h3>
-  <h3>ZARA</h3>
-  <h3>GUCCI</h3>
-  <h3>PRADA</h3>
-  <h3>CALVIN KLEIN</h3>
-
-    </div>
-
     </div>
   )
 }
-
 
 export default Omina
